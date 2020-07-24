9267031ffb07c09ca0281dd05719baf4914a5683
dibyanshushekhardey <dibyanshushekhardey@gmail.com>
2020-07-24 19:07:49 +0530

    Added check_disk_full and more parameters to main function

--- week_1/codes/all_checks.py
+++ week_1/codes/all_checks.py
@@ -1,11 +1,33 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 
 import os
+import shutil
+import sys
 
 def check_reboot():
   """Returns True if the computer has a pending reboot"""
   return os.path.exist("/run/reboot-required")
+
+def check_disk_full(disk, min_absolute, min_percent):
+  """Returns True if there isn't enough disk space, False otherwise."""
+  du = shutil.disk_usage(disk)
+  #Calculate the percentage of free space
+  percent_free = 100 * du.free / du.total
+  # Calculate how many free gigabytes
+  gigabytes_free = du.free / 2**30
+  if percent_free < min_percent or gigabytes_free < min_absolute:
+    return True
+  return False
+
 def main():
+  if check_reboot():
+    print("Pendong Reboot.")
+    sys.exit(1)
+  if check_disk_full("/", 2, 10):
+    print("Disk full.")
+    sys.exit(1)
+
+main()
   pass
 
 main()
