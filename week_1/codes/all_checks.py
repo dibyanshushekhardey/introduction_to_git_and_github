d35035e7de8f6ae27aa309efc9b4525a9789dcc6
dibyanshushekhardey <dibyanshushekhardey@gmail.com>
2020-07-24 19:38:16 +0530

    Create wrapper function for check_disk_full

--- week_1/codes/all_checks.py
+++ week_1/codes/all_checks.py
@@ -6,9 +6,9 @@ import sys
 
 def check_reboot():
   """Returns True if the computer has a pending reboot"""
-  return os.path.exist("/run/reboot-required")
+  return os.path.exists("/run/reboot-required")
 
-def check_disk_full(disk, min_absolute, min_percent):
+def check_disk_full(disk, min_gb, min_percent):
   """Returns True if there isn't enough disk space, False otherwise."""
   du = shutil.disk_usage(disk)
   #Calculate the percentage of free space
@@ -19,12 +19,19 @@ def check_disk_full(disk, min_absolute, min_percent):
     return True
   return False
 
+def check_root_full():
+  """Return True if the root partition is full, False otherwise"""
+  return check_disk_full(disk="/", min_gb=2, min_percent=10)
+
 def main():
   if check_reboot():
-    print("Pendong Reboot.")
+    print("Pending Reboot.")
     sys.exit(1)
-  if check_disk_full(disk="/", min_gb=2, min_percent= 10):
-    print("Disk full.")
+  if check_root_full():
+    print("Root partition full.")
     sys.exit(1)
+
+  print("Everything is OK.")
+  sys.exit(0)
 
 main()
